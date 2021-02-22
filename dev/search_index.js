b58161c219775b7afe7da14fc6e07c621dc5ef81
Documenter.jl <documenter@juliadocs.github.io>
2021-02-22 15:53:28 +0000

    build based on 9c0dee4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeidSolarScripts","category":"page"},{"location":"#NeidSolarScripts","page":"Home","title":"NeidSolarScripts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeidSolarScripts]","category":"page"}]
+}
