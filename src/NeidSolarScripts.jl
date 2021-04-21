01b5ee7566af71220d75cd7b5564f2601c8fc3f3
Eric Ford <ebf11@psu.edu>
2021-04-21 11:58:20 -0400

    Adding differential extinction code based on Andrea's python script (still need to check case of small difference)

--- src/NeidSolarScripts.jl
+++ src/NeidSolarScripts.jl
@@ -12,6 +12,7 @@ export CalculateFWHMDifference_SolarRotation_from_long_lat_alt, CalculateFWHMDif
 #export get_wiyn_loc, get_harpsn_loc
 
 # For computing differential extinction
-
+include("diff_solar_extinction.jl")
+export DifferentialExtinction
 
 end
