edbcaf819d5d70dae9953084363452b40239916d
Eric Ford <ebf11@psu.edu>
2021-04-21 23:44:16 -0400

    using pip for astroquery

--- deps/pyimports.jl
+++ deps/pyimports.jl
@@ -1,18 +1,18 @@
 ENV["PYTHON"] = ""
 using PyCall
 using Conda
+
+pipcmd = joinpath(Conda.PYTHONDIR,"pip")
+#Conda.add("astroquery.jplhorizons")
+#Conda.add("astroquery")
+run(`$pipcmd install --pre astroquery`)
+
 Conda.add("astropy")
 #Conda.add("astropy.coordinates")
 #Conda.add("astropy.time")
-#Conda.add("astroquery.jplhorizons")
-Conda.add("astroquery")
 Conda.add("barycorrpy")
 #Conda.add("scipy.spatial.transform")
 Conda.add("scipy")
 
-#=
-using Conda
-pipcmd = joinpath(Conda.PYTHONDIR,"pip")
-run(`$pipcmd install pyneid`)
-=#
+#run(`$pipcmd install pyneid`)
 
