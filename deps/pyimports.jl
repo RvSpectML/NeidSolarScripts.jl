b3590f4f9f9bb71770abc08de360ec0bda70f7a1
Eric Ford <ebf11@psu.edu>
2021-04-21 23:49:33 -0400

    pip for barycorrpy

--- deps/pyimports.jl
+++ deps/pyimports.jl
@@ -3,16 +3,19 @@ using PyCall
 using Conda
 
 pipcmd = joinpath(Conda.PYTHONDIR,"pip")
+
 #Conda.add("astroquery.jplhorizons")
-#Conda.add("astroquery")
-run(`$pipcmd install --pre astroquery`)
+Conda.add("astroquery", channel="conda-forge")
+#run(`$pipcmd install --pre astroquery`)
+
+run(`$pipcmd install barycorrpy`)
 
 Conda.add("astropy")
 #Conda.add("astropy.coordinates")
 #Conda.add("astropy.time")
-Conda.add("barycorrpy")
-#Conda.add("scipy.spatial.transform")
+
 Conda.add("scipy")
+#Conda.add("scipy.spatial.transform")
 
 #run(`$pipcmd install pyneid`)
 
