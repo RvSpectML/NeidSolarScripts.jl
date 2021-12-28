1afdf300654e40b3a2444c4c149306dd68d07e04
Eric Ford <ebf11@psu.edu>
2021-12-27 19:39:26 -0500

    flatten tomls into dataframe

--- examples/combine_daily_reports_v1.0.jl
+++ examples/combine_daily_reports_v1.0.jl
@@ -1,7 +1,6 @@
 using Logging
 using ArgParse
 
-
 @info "# Parsing arguments..."
  function parse_commandline()
      s = ArgParseSettings( description = "Combined NEID solar daily reports into one csv file.")
@@ -19,6 +18,9 @@ using ArgParse
              help = "Filename for daily reports (toml)"
              arg_type = String
              default = "daily_summary.toml"
+         "--exclude_filename"
+             help = "Filename with dates to exclude (csv)"
+             arg_type = String
          "--overwrite"
             help = "Specify it's ok to overwrite the output file."
             #default = true
@@ -62,16 +64,77 @@ files3 = glob([r"\d{4}",r"\d{2}",r"\d{2}",args["input_filename"]],args["input_pa
 files = vcat(files1,files2,files3)
 @assert length(files) >= 1
 
-#@info "# Parsing daily toml files."  num_files=length(files)
+if !isnothing(args["exclude_filename"]) && isfile(args["exclude_filename"]) && (filesize(args["exclude_filename"])>0)
+   @info "# Reading days to exclude."
+   df_exclude = CSV.read(args["exclude_filename"],DataFrame)
+   @info "# Found $(size(df_exclude,1)) dates to exclude"
+else
+   df_exclude = DataFrame()
+end
+
+#=
+function dates_to_strings!(d::AbstractDict)
+   for (k,v) in pairs(d)
+      if typeof(v) <: Date
+         d[k] = string(v)
+      elseif typeof(v) <: DateTime
+         d[k] = string(v)
+      elseif typeof(v) <: AbstractDict
+         dates_to_strings!(d[k])
+      end
+   end
+   return d
+end 
+=#
+
+function _flatten_dict!(d_out::D1, d_in::D2; prestr::String = "") where { D1<:AbstractDict,  D2<:AbstractDict } 
+   for (k1,v1) in pairs(d_in)
+       if typeof(v1) <: AbstractDict
+          _flatten_dict!(d_out,v1, prestr = prestr * k1 * "." ) 
+       else
+          d_out[prestr * k1 ] = v1
+       end
+   end
+   return d_out
+end
+
+function flatten_dict(d::D) where { D<:AbstractDict } 
+   d_out =  D()   
+   _flatten_dict!(d_out, d)
+end
+
+function array_of_dict_to_dataframe( aod::AOD; first_cols::AbstractVector{String}=String[], exclude_cols::AbstractVector{String}=String[] ) where { D<:AbstractDict, AOD<:AbstractVector{D} }
+  df = DataFrame()
+  for k in first_cols
+      if !haskey(first(aod), k) || in(k, exclude_cols)
+         continue 
+      end 
+      df[!,k] = map(i->aod[i][k],1:size(aod,1)) 
+  end
+  for k in sort!(collect(keys(first(aod))))
+      if in(k, first_cols) || in(k, exclude_cols) 
+         continue
+      end
+      df[!,k] = map(i->aod[i][k],1:size(aod,1)) 
+  end
+  return df
+end
+
+@info "# Parsing daily toml files."  num_files=length(files)
 daily = Vector{Dict{String,Any}}(undef, size(files,1) )
 println("files = ", files)
 flush(stdout)
 flush(stderr)
 j = 0
+parser = TOML.Parser()
 for file in files
     if filesize(file) >0 
        @info "# Processing $file"
-       d = TOML.parsefile(file) 
+       d = TOML.parsefile(parser,file) 
+       if haskey(d,"obs_date") && haskey(d["obs_date"],"string") && (size(df_exclude,1)>=1) && ( in(d["obs_date"]["string"], df_exclude.date_to_exclude) )
+          continue
+       end
+       #dates_to_strings!(d)
        global j += 1
        daily[j] = d
     end
@@ -79,9 +142,17 @@ end
 num_days_with_usable_obs = j
 resize!(daily,num_days_with_usable_obs)
 
-
 @info "# Making dataframe"
-df = DataFrame()
+daily_flat = flatten_dict.(daily)
+first_cols = ["obs_date.string", "obs_date.mean_bjd","num_rvs.good"]
+cols_to_exclude = ["report.hostname", "title", "report.input_md5", "report.input_file"]
+df =  array_of_dict_to_dataframe( daily_flat, first_cols=first_cols, exclude_cols=cols_to_exclude )
+
+@info "# Sorting dataframe"
+df_sorted = sort!(df,Symbol("obs_date.mean_bjd") ) 
+
+#=
+df.obs_date = map(day->day["obs_date"]["string"], daily)
 df.obs_date = map(day->day["obs_date"]["string"], daily)
 df.mean_bjd = map(day->day["obs_date"]["mean_bjd"], daily)
 df.num_rvs_usable = map(day->day["num_rvs"]["usable"], daily)
@@ -92,9 +163,9 @@ df.median_σ_rv = map(day->day["rv"]["drp"]["median_σ_rv"], daily)
 df.rms_rv_drp = map(day->day["rv"]["drp"]["rms_rvs"], daily)
 df.winsor_mean_rv_drp = map(day->day["rv"]["drp"]["winsor_mean_rv"], daily)
 df.winsor_rms_rv_drp = map(day->day["rv"]["drp"]["winsor_rms_rv"], daily)
-
 @info "# Sorting dataframe"
 df_sorted = df |> @orderby( _.mean_bjd ) |> DataFrame
+=#
 
 @info "# Writing CSV file"
 CSV.write(output_fn,df_sorted )
